9c478be90293c403a4f6a456aa5fbce6beed1279
Nikita <nikitalux01@gmail.com>
2020-10-02 16:58:34 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 6 ;
+console.log(number);
+
+const obj={
+    a:50
+};
+obj.a=10;
+console.log(obj);
+
+
+
+
+var name = 'Ivan';
+console.log(name);
+
+{
+ var result=50;
+}
+
+console.log(result);
+
+alert(5);
+number = 4.6;
+
+
+
