9d5702f74d5070be21ede78d23253d8df953115a
Nikita <nikitalux01@gmail.com>
2020-10-06 13:09:34 +0300

    first task js

--- js/script.js
+++ js/script.js
@@ -2,32 +2,54 @@
 
 
 
-let number = 5;
-const leftBorderWidth = 1;
+let numberOfFilms=+prompt("Сколько фильмов Вы уже посмотрели?");
 
-number = 6 ;
-console.log(number);
 
-const obj={
-    a:50
+let personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false  ,
 };
-obj.a=10;
-console.log(obj);
+
+console.log(numberOfFilms);
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+// const leftBorderWidth = 1;
+
+// number = 6 ;
+// console.log(number);
+
+// const obj={
+//     a:50
+// };
+// obj.a=10;
+// console.log(obj);
 
 
 
 
-var name = 'Ivan';
-console.log(name);
+// var name = 'Ivan';
+// console.log(name);
 
-{
- var result=50;
-}
+// {
+//  var result=50;
+// }
 
-console.log(result);
+// console.log(result);
 
-alert(5);
-number = 4.6;
+// alert(5);
+// number = 4.6;
 
 
 
